d3c23fe341cede2b042e2beb946f9a991796e094
Dominik Dewitz <dewitzdominik@kabelmail.de>
2021-06-29 16:08:17 +0200

    Add Redux State, set routes

--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -1,13 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 
-import HelloWorld from '../components/HelloWorld';
-
-class App extends React.Component {
-  render() {
-    return <>
-        <HelloWorld />
-      </>
-  }
-}
