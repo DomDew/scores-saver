aa6c3c91968ce13ee48c24c988410566bf265939
Dominik Dewitz <dewitzdominik@kabelmail.de>
2021-07-06 15:34:59 +0200

    Create user_score method

--- app/models/api/match.rb
+++ app/models/api/match.rb
@@ -2,4 +2,9 @@ class Api::Match < ApplicationRecord
   has_many :player_scores, foreign_key: 'api_match_id'
 
   belongs_to :user
+
+  # Allow to access games for the current_user to display statistics more easily
+  def user_score
+    player_scores.where(owner: true)
+  end
 end
