5c1c528bd84fc3800992c9c215d3075fa5911780
Dominik Dewitz <dewitzdominik@kabelmail.de>
2021-07-07 14:26:52 +0200

    Write tests for Match model

---
+++ spec/models/api/match_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Api::Match, type: :model do
+  let (:user) { build_user }
+
+  it 'Returns a valid match' do
+    match = build(:match)
+
+    expect(match).to be_present
+  end
+
+  context 'When creating a new match' do
+    it 'Returns a valid match' do
+      created_match = create(:match, user: user)
+
+      expect(created_match).to be_present
+    end
+  end
+end
